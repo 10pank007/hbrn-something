15315bc1f6a2a2481c489d75de1df1fcd762b3a7
Bohdan Pankiv <pankivbogdan96@gmail.com>
2024-01-22 15:54:06 +0200

    1 preview hibernate

---
+++ src/main/java/models/User.java
@@ -0,0 +1,47 @@
+package models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
+
+@Entity
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //auto increment id
+    private int id;
+    private String name;
+
+    public User() {
+    }
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,47 @@
 package org.example;
 
+import models.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+        Metadata metadata = new MetadataSources(serviceRegistry)
+                .addAnnotatedClass(User.class)
+                .getMetadataBuilder()
+                .build();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        //crud
+//        User vasil = new User("vasil");
+//        User alexandr = new User("alexandr");
+//        User volodimir = new User("volodimir");
+//        User vitaliy = new User("vitaliy");
+//        session.save(vasil);
+//        session.save(alexandr);
+//        session.save(volodimir);
+//        session.save(vitaliy);
+//        User user = session.find(User.class, 1);
+//        System.out.println(user);
+//        List<User> list = session.createNativeQuery("select * from user", User.class).list();
+        List<User> list = session.createQuery("select u from User u", User.class).list();
+        System.out.println(list);
+
+        session.getTransaction().commit();
+        session.close();
+        sessionFactory.close();
+        serviceRegistry.close();
     }
 }
